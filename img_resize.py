5a8e665e106a5788680f78d5a8c0eb9ce34279e6
Chris Yunbin Chang <hw@yunbin.kr>
2018-08-24 20:45:40 +0900

    make 500_squared_dataseat

--- img_resize.py
+++ img_resize.py
@@ -7,5 +7,5 @@ num = 0
 for img_path in imglist:
     img = Image.open(img_path)
     print(img_path, img.size, "./resize/resize" + str(num) + ".jpg")
-    img.resize((600, 600)).save("./resize/resize" + str(num) + ".jpg")
+    img.resize((600, 600)).save("./resize_500_square/resize" + str(num) + ".jpg")
     num = num + 1

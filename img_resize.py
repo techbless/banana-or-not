3322fbfbe01ec3ee9abf4ea2ac0dcf723c388c60
Chris Yunbin Chang <hw@yunbin.kr>
2018-08-24 20:43:09 +0900

    delete no acceptable, add dataset zip

---
+++ img_resize.py
@@ -0,0 +1,11 @@
+import glob
+from PIL import Image
+
+imglist = glob.glob("./banana_dataset/*.jpg")
+
+num = 0
+for img_path in imglist:
+    img = Image.open(img_path)
+    print(img_path, img.size, "./resize/resize" + str(num) + ".jpg")
+    img.resize((600, 600)).save("./resize/resize" + str(num) + ".jpg")
+    num = num + 1

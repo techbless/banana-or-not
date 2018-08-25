5d9e46cddec0689c0810b63dcdcd50754ad171d1
Chris Yunbin Chang <hw@yunbin.kr>
2018-08-25 12:54:01 +0900

    add other dataset(with desk, road)

--- img_resize.py
+++ img_resize.py
@@ -1,11 +1,14 @@
 import glob
 from PIL import Image
 
-imglist = glob.glob("./banana_dataset/*.jpg")
+imglist = glob.glob("./other_dataset/*.jpg")
 
 num = 0
 for img_path in imglist:
-    img = Image.open(img_path)
-    print(img_path, img.size, "./resize/resize" + str(num) + ".jpg")
-    img.resize((600, 600)).save("./resize_500_square/resize" + str(num) + ".jpg")
-    num = num + 1
+    try:
+        img = Image.open(img_path)
+        print(img_path, img.size, str(num) + ".jpg")
+        img.resize((250, 250)).save("./o_resize_250_square/resize" + str(num) + ".jpg")
+        num = num + 1
+    except:
+        print("Error Occur")
